174fe805243afc0caec61f3da22e6275854d4e9f
dozerozturk <ozturkozer@yahoo.com>
2023-01-25 14:08:19 -0600

    update

---
+++ src/week14/polymorphism/hospital/Doctor.java
@@ -0,0 +1,9 @@
+package week14.polymorphism.hospital;
+
+public class Doctor extends Employee{
+    @Override
+    public void performJob() {
+        System.out.println("I am performing doctor job");
+
+    }
+}
